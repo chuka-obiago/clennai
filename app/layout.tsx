8c44979a5e7caf92581b4a93c0bf340e24ca5f0a
Ekene Obiago <chukaobiago@gmail.com>
2026-06-19 18:29:22 +0100

    Added Umami script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,23 @@
+import Script from "next/script";
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import StyledJsxRegistry from "./registry";
+
+export const metadata: Metadata = {
+  title: "ClennAI | AI Systems for Businesses",
+  description:
+    "ClennAI helps businesses automate lead generation, outreach, and sales pipelines using AI-powered systems.",
+
+  openGraph: {
+    title: "ClennAI | AI for Businesses",
+    description:
+      "ClennAI helps businesses automate lead generation, outreach, and sales pipelines using AI-powered systems.",
+    images: "/opengraph-image.png",
+    type: "website",
+    url: "https://clennai.com"
+  },
+};
 
 export default function RootLayout({
   children,
@@ -12,6 +29,12 @@ export default function RootLayout({
       <body>
         <Navbar />
         <StyledJsxRegistry>{children}</StyledJsxRegistry>
+
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="489b5533-d441-4ee2-aa9f-fca980976615"
+        />
       </body>
     </html>
   );
